98d750509425775e31f9f1f0c3aa210a9d5937a0
mike2sull <mike2sull@gmail.com>
2016-06-28 20:46:32 -0400

    Completed thorugh ex5!

---
+++ ex3.py
@@ -0,0 +1,25 @@
+print "I will no wcount my chickens:"
+
+#First divides 30 by 6, then adds 25
+print "Hens", 25+30/6
+#Multiplies 25 by 3 then divides by 4 and takes the remainder (3) and subtracts it from 100 
+print "Roosters", 100-25*3%4
+print "Now I will count the eggs:"
+
+#Follows the PEMDAS order of operations
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3+2<5-7
+
+print "What is 3 + 2?", 3+2
+print "What is 5-7?", 5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more?"
+
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5 <= -2
